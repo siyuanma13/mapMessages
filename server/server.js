287f897e11bbc11356582d039eebeb1c0eea58eb
Siyuan Ma <sma232@wisc.edu>
2018-12-25 17:21:32 -0600

    first commit

---
+++ server/server.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    return res.send('Hello world');
+});
+
+app.listen(process.env.PORT || 8080);
