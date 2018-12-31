8540c2abd823a79aa22c0e561230dc0b4c59822e
Siyuan Ma <sma232@wisc.edu>
2018-12-31 17:11:37 -0600

    added some server side code

--- server/server.js
+++
@@ -1,9 +0,0 @@
-// JavaScript source code
-const express = require('express');
-const app = express();
-
-app.get('/', function (req, res) {
-    return res.send('Hello world');
-});
-
-app.listen(process.env.PORT || 8080);
